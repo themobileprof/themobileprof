a7752b1e1cf7940bc384ee4753c34ee0df6c15e4
sanyaele <samuelanyaele@gmail.com>
2020-09-05 00:48:30 +0100

    debug files.php

--- files/files.php
+++ files/files.php
@@ -1,14 +1,12 @@
 <?php
-if (isset($_GET['url'])) {
-	$url = "http://35.192.0.70/content.php";
-	$ch = curl_init();
-	$timeout = 5;
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-	$data = curl_exec($ch);
-	curl_close($ch);
-}
+$url = "http://35.192.0.70/content.php";
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$data = curl_exec($ch);
+curl_close($ch);
 
 
 ?>
