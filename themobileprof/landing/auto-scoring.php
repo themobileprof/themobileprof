04b61a3dbdfc7193efb76e34e3d5725a9e4495b0
sanyaele <samuelanyaele@gmail.com>
2020-10-05 10:54:20 +0100

    work in progress

--- themobileprof/landing/auto-scoring.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("HTTP/1.1 301 Moved Permanently");
-if (!empty($_REQUEST['target'])){
-    header("Location: http://themobileprof.com/landing/auto-scoring.php?target=NG");
-} else {
-    header("Location: http://themobileprof.com/landing/auto-scoring.php");
-}
-header("Connection: close");
-?>
