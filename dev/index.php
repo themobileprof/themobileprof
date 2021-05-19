3fcad22f9973b298a74c7641244259dc344c659e
Samuel <samuelanyaele@gmail.com>
2021-05-19 01:32:11 +0100

    Edit theMobileProf

--- dev/index.php
+++ dev/index.php
@@ -17,11 +17,8 @@
 
     <link rel="stylesheet" href="css/jquery.fancybox.min.css">
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-
     <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
 
-    <link rel="stylesheet" href="css/aos.css">
 
     <link rel="stylesheet" href="css/style.css">
     <script>
