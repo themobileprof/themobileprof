2fd9e56687fac036209f2bd0329c66b5e46f066b
Samuel <samuelanyaele@gmail.com>
2021-05-27 04:12:54 +0100

    new files

---
+++ mobileops.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-153265481-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-153265481-1');
+	</script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-7718160-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-7718160-1');
+	</script>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="Samuel Anyaele">
+
+	<title>MobileOps Training</title>
+
+
+
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Font Awesome CSS -->
+	<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="css/modern-business.css" rel="stylesheet">
+
+	<link href='//fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="shortcut icon" type="image/x-icon" href="assets/favicon.png">
+
+</head>
+
+<body>
+
+
+	<!-- Navigation -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="index.html"><img src="assets/logo.jpg"></a>
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link" href="index.html">Home</a>
+					</li>
+
+					<li class="nav-item">
+						<a class="nav-link" href="training.html">Online Courses</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="about.html">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="contact.html">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<!-- Page Content -->
+	<div class="container py-5">
+
+		<!-- Page Heading/Breadcrumbs -->
+		<h1 class="mt-4 mb-3">6 Weeks Linux Code Camp - Level 1
+		</h1>
+
+		<ol class="breadcrumb bg-white">
+			<li class="breadcrumb-item">
+				<a href="index.html">Home</a>
+			</li>
+			<li class="breadcrumb-item">
+				<a href="training.html">Online Courses</a>
+			</li>
+
+			<li class="breadcrumb-item active">Linux - Level 1</li>
+
+		</ol>
+
+		<!-- Portfolio Item Row -->
+		<div class="row">
+
+			<div class="col-md-8">
+				<div class="row">
+
+					<p class="firstp">
+						<img src="assets/coder.png" alt="Programmer" class="img-fluid">
+
+						<div>
+							<h3>Reserve a Seat Now</h3>
+							<a href="tel:+2348033954301" class="btn btn-danger m-1 text-white">
+								Reserve a Seat</a>
+						</div>
+
+						<table class="table table-bordered">
+							<thead class="thead-dark">
+								<tr>
+									<th>6 Weeks</th>
+									<th>Topics</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Week 1</td>
+									<td>Linux and Neovim</td>
+
+								</tr>
+								<tr>
+									<td>Week 2</td>
+									<td>Bash Scripting</td>
+								</tr>
+								<tr>
+									<td>Week 3</td>
+									<td>Python Scripting</td>
+								</tr>
+								<tr>
+									<td>Week 4</td>
+									<td>GIT</td>
+								</tr>
+								<tr>
+									<td>Week 5</td>
+									<td>SQLite</td>
+								</tr>
+								<tr>
+									<td>Week 6</td>
+									<td>MySQL</td>
+								</tr>
+								<tr>
+									<td>Week 7</td>
+									<td>Projects</td>
+								</tr>
+
+							</tbody>
+
+						</table>
+						<div class="row p-3">
+							<div class="">
+								<div class="m-2"><img src="assets/slide/7.png" class="img-fluid" alt=""></div>
+								<div class="m-2"><img src="assets/slide/8.png" class="img-fluid" alt=""></div>
+								<div class="m-2"><img src="assets/slide/9.png" class="img-fluid" alt=""></div>
+								<div class="m-2"><img src="assets/slide/10.png" class="img-fluid" alt=""></div>
+								<div class="m-2"><img src="assets/slide/12.png" class="img-fluid" alt=""></div>
+								<div class="m-2"><img src="assets/slide/13.png" class="img-fluid" alt=""></div>
+							</div>
+						</div>
+
+				</div>
+
+
+
+				<div id="details" class="row mt-4 mb-4">
+					<div class="bg-primary col-md-12 text-white pt-3 mb-3 rounded-top">
+
+						<h3 class="text-light">Online Classes</h3>
+
+					</div>
+					<div class="col-md-7">
+						<div>
+							<div class="display-4 text-success">N80,000</div>
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item"><strong>Duration:</strong> 10 Weeks</li>
+							<li class="list-group-item"><strong>Days:</strong> Monday to Friday(1 Hour Daily)</li>
+						</ul>
+					</div>
+					<div class="benefits col-md-5">
+						<h3 class="text-danger">To Qualify for the class, you first have to take and pass an entry assessment test</h3>
+						<!-- <div data-toggle="modal" data-target="#payModal"> -->
+						<div>
+							<a href="https://docs.google.com/forms/d/e/1FAIpQLSdUlOzfoIqPMWwnGE4KKfs7ZjNEeHDPR_N0Zzrp1mwSnqGimA/viewform?usp=sf_link" class="btn btn-primary m-1 text-white">
+								Take Entry test</a>
+						</div>
+
+
+
+					</div>
+
+
+				</div>
+
+
+			</div>
+
+			<div class="col-md-4">
+				<div class="embed-responsive embed-responsive-1by1">
+
+					<video class="embed-responsive-item" controls>
+						<source src="assets/php_sample.mp4 " frameborder="0" allowfullscreen></video>
+
+				</div>
+				<div class="card mt-2">
+
+					<div class="card-body hidlist">
+						<h4 class="pt-2 text-primary">Students in our <strong>UniLag</strong> Code Class get free <strong class="text-dark">Git and Google Cloud Console</strong> classes</h4>
+						<img src="assets/unilagos.jpg" alt="" style="width: 100px;" class="pull-left m-2">
+						<p>If you pass the test you get an <b>additional 20%</b> discount on the course or <b>2 weeks free training</b> on Terminal based Git and Gcloud management commands</p>
+
+					</div>
+					<div class="card-footer text-muted">
+						To qualify for the free classes, <a href="https://docs.google.com/forms/d/e/1FAIpQLSdUlOzfoIqPMWwnGE4KKfs7ZjNEeHDPR_N0Zzrp1mwSnqGimA/viewform?usp=sf_link">take and pass our entry test</a>
+					</div>
+				</div>
+				<div class="card mt-2">
+					<div class="card-header bg-primary">
+						<h4 class="text-white pt-2">Benefits of the Course</h4>
+					</div>
+
+					<div class="card-body hidlist">
+						<div><i class="fa fa-check-square text-primary"></i>
+							You will no longer be terrified of the black screen of the Linux terminal</div>
+						<div><i class="fa fa-check-square text-primary"></i>Learn how to manipulate files and directories within Linux on Mobile, Desktop, and Server</div>
+						<div><i class="fa fa-check-square text-primary"></i>Connect into and work in your Cloud Server through SSH and SFTP</div>
+
+						<div><i class="fa fa-check-square text-primary"></i>Learn how to use command line editors for web application development, giving you the skills to work in most computers you login to, physically or remotely</div>
+
+						<div><i class="fa fa-check-square text-primary"></i>Master how to develop web applications with your Smartphone - using Termux, NEOVim, and mobile browsers</div>
+						<div><i class="fa fa-check-square text-primary"></i>Use git and github as version control system and remote repo to manage updates on your web application</div>
+						<div>
+
+							<hr>
+							<div>For our Advanced Laravel and VueJS Classes, call us for details</div>
+							<a href="tel:+234-803-395-4301" class="btn btn-primary m-1">
+								Call to Register for Class</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+		<!-- /.row -->
+
+
+	</div>
+	<!-- /.container -->
+
+	<!-- Pay Modal-->
+	<div class="modal fade" id="payModal" tabindex="-1" role="dialog" aria-labelledby="payModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="payModalLabel">Mobile Web Design Class</h5>
+					<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">×</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<iframe src="" name="pay_frame" id="pay_frame" frameborder="0"></iframe>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<footer class="py-5 bg-dark">
+		<div class="container">
+			<p class="m-0 text-center text-white">Copyright &copy; TheMobileProf.com 2019</p>
+		</div>
+		<!-- /.container -->
+	</footer>
+
+	<!-- Bootstrap core JavaScript -->
+	<script src="vendor/jquery/jquery.min.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
